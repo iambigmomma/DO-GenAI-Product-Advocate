d1c755c0e19fc63a2c0f05e6543e7ee67752f9fa
Jeff Fan <jfan@digitalocean.com>
2025-02-18 17:56:11 +0100

    add chatbot

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,32 @@
 import "../public/styles.css";
+import { useEffect } from "react"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+      const script = document.createElement("script")
+      script.src =
+        "https://agent-4beb4b58203d8395def9-czxvl.ondigitalocean.app/static/chatbot/widget.js"
+      script.async = true
+
+      // Chatbot configuration
+      script.dataset.agentId = "afae3663-eaae-11ef-bf8f-4e013e2ddde4"
+      script.dataset.chatbotId = "DfwJnJJlo3ufHdG8E3qZ8_yAykiawWtU"
+      script.dataset.name = "DigitalOcean Product Advocate"
+      script.dataset.primaryColor = "#031B4E"
+      script.dataset.secondaryColor = "#000000"
+      script.dataset.buttonBackgroundColor = "#FFFFFF"
+      script.dataset.startingMessage =
+        "Welcome to DigitalOcean! I am Nemo, specialized in our products! I will recommend the most suitable product portfolio based on your request!"
+      script.dataset.logo =
+        "https://pbs.twimg.com/media/EGmxMG8XUAA-pO3.png:large"
+
+      document.body.appendChild(script)
+
+      return () => {
+        document.body.removeChild(script) // Clean up script on unmount
+      }
+    }, [])
+
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
       {/* left side */}
       <div className="lg:w-1/2 relative z-10 flex flex-col justify-center px-10 lg:px-20 py-20 lg:py-0 text-left">
         <h2 className="inter text-4xl mb-3 font-bold text-gray-800">
-          Welcome to Your Next.js App{" "}
+          DigitalOcean Solution Architecture Center{" "}
           <span className="block text-blue-500 text-2xl font-normal">
-            Deployed to DigitalOcean
+            Deployed the DeepSeek Model on GenAI Platform!
           </span>
         </h2>
 
@@ -18,16 +18,9 @@ export default function Home() {
 
         <div className="sm:flex">
           <a
-            href="https://www.digitalocean.com/docs/app-platform"
-            className="block py-2 px-5 rounded shadow bg-gray-500 text-gray-100 sm:mr-2 mb-2 sm:mb-0"
-          >
+            href="https://docs.digitalocean.com/products/genai-platform/"
+            className="block py-2 px-5 rounded shadow bg-gray-500 text-gray-100 sm:mr-2 mb-2 sm:mb-0">
             View the Docs
-          </a>
-          <a
-            href="https://cloud.digitalocean.com/apps"
-            className="block py-2 px-5 rounded shadow bg-blue-500 text-blue-100"
-          >
-            View Your Dashboard
           </a>
         </div>
       </div>
@@ -37,17 +30,18 @@ export default function Home() {
         <svg
           className="hidden lg:block text-white fill-current absolute h-full transform -translate-x-1/2 w-48 z-10"
           viewBox="0 0 100 100"
-          preserveAspectRatio="none"
-        >
+          preserveAspectRatio="none">
           <polygon points="50,0 100,0 50,100 0,100"></polygon>
         </svg>
 
         <Image
-          src="background.jpg"
-          alt="Ocean Image"
+          src="https://res.cloudinary.com/iambigmomma/image/upload/v1739274188/banner/deepseek-deploy-in-3-ways.jpg"
+          alt="DigitalOcean and DeepSeek"
+          layout="fill"
+          objectFit="cover"
           className="lg:absolute object-cover lg:inset-y-0 lg:right-0 lg:h-full lg:w-full"
         />
       </div>
     </div>
-  );
+  )
 }
